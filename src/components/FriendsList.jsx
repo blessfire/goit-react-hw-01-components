cf29b9c1fcfef5b9c51ab734da3a905e3c570784
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-08-29 15:31:09 +0200

    add data start profile

---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,13 @@
+import friends from 'data/friends.json';
+
+{/* <FriendList friends={friends} />; */}
+
+{/* <ul class="friend-list">
+  <!-- Довільна кіл-сть FriendListItem -->
+</ul> */}
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
