d893921900d916223e999efa2c0d71371350be84
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-08-30 15:06:03 +0200

    add component Friends to be continue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import { Profile } from 'components/Profile/Profile';
-import { Statistics} from 'components/Statistics/Statistics';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendsList } from 'components/Friends/FriendsList';
 import user from 'data/user.json';
 import data from 'data/data.json';
-// import friends from 'data/friends.json';
+import friends from 'data/friends.json';
 // import transactions from 'data/transactions.json';
 
 
@@ -10,14 +11,14 @@ export const App = () => {
   return (
     <div 
     style = {{
-        height: '100vh',
+        minHeight: '100vh',
       width:'768px',
         display: 'flex',
         flexDirection: 'column',
      flexWrap: 'noWrap',
      justifyContent: 'center',
         alignItems: 'center',
-        padding: '80px',
+        padding: '20px 80px',
      fontSize: 40,
      color: '#010101'
       }}
@@ -30,6 +31,10 @@ export const App = () => {
   stats={user.stats}/>
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
+      <FriendsList friends={friends}
+        // avatar={friends.avatar} name={friends.name}
+        // isOnline={friends.isOnline} key={friends.id}
+      />
     </div>
 
 );
--- src/components/Friends/FriendsList.jsx
+++ src/components/Friends/FriendsList.jsx
@@ -1,14 +1,34 @@
 import PropTypes from 'prop-types';
+import css from './Friends.module.css'
 
+export const FriendsList = ({friends}) => {
+  return (
+    <div className={css.friends_box}>
+      <ul className={css.friend_list}>
+        {friends.map(({avatar, name, isOnline,id}) => (
+          <li className={css.item} key={id} >
+            <span className={css.status} style={{ backgroundColor: isOnline ? 'green' : 'red' }}>{ isOnline }</span>
+    <img src={avatar} alt={`${name}_avatar`} width={48} className={css.avatar} />
+            <p className={css.name}>{name}</p>
+    </li>))  }</ul>
+  </div>
+  )
+}
 
-{/* <FriendList friends={friends} />; */}
-
-{/* <ul class="friend-list">
-  <!-- Довільна кіл-сть FriendListItem -->
-</ul> */}
-
-<li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li>
+// FriendsList.propTypes = {
+//   friends:
+//     PropTypes.arrayOf(PropTypes.shape({
+//       avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+//   id:PropTypes.number.isRequired,
+//   })).isRequired,
+// };
+FriendsList.protoTypes = {
+  friends:PropTypes.arrayOf(PropTypes.exact({
+    avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id:PropTypes.number.isRequired,
+  }).isRequired)
+}
