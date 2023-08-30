84d7000ac91be4d3f59f61397b5adfa75e44a66e
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-08-30 08:45:06 +0200

    statistic done!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,16 @@ import data from 'data/data.json';
 
 export const App = () => {
   return (
-   <div style={{
+    <div 
+    style = {{
      height: '100vh',
      display: 'flex',
      justifyContent: 'center',
      alignItems: 'center',
      fontSize: 40,
      color: '#010101'
-    }}>
+      }}
+    >
       {/* <Profile username={username} /> */}
       <Statistics data={data} />
       
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,10 +2,12 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({ data }) => {
-  return (<ul className={css.stat_list}> {data.map((stat) => <li key={stat.id} className={css.item}><span className={css.label}>{stat.label}</span>
+  return (< section className={css.statistics} >
+    <h2 className={css.title}>Upload stats</h2>
+    <ul className={css.stat_list}> {data.map((stat) => <li key={stat.id} className={css.item}><span className={css.label}>{stat.label}</span>
      
     <span className={css.percentage}>{stat.percentage}%</span></li>)}
-  </ul>)
+  </ul></section>)
 }
 
 Statistics.propTypes = {
